ec0dcbdb4df58f57bd7cbefc946c5d0c1bc68421
Abhishek Thakur <abhishek4@gmail.com>
2017-02-27 13:59:07 +0100

    add nnet

---
+++ deepnet.py
@@ -0,0 +1,176 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.embeddings import Embedding
+from keras.layers.recurrent import LSTM, GRU
+from keras.layers.normalization import BatchNormalization
+from keras.utils import np_utils
+from keras.engine.topology import Merge
+from keras.layers import TimeDistributed, Lambda
+from keras.layers import Convolution1D, GlobalMaxPooling1D
+from keras.callbacks import ModelCheckpoint
+from keras import backend as K
+from keras.layers.advanced_activations import PReLU
+from keras.preprocessing import sequence, text
+
+data = pd.read_csv('quora_duplicate_questions.tsv', sep='\t')
+y = data.is_duplicate.values
+
+tk = text.Tokenizer(nb_words=200000)
+
+max_len = 40
+tk.fit_on_texts(list(data.question1.values) + list(data.question2.values.astype(str)))
+x1 = tk.texts_to_sequences(data.question1.values)
+x1 = sequence.pad_sequences(x1, maxlen=max_len)
+
+x2 = tk.texts_to_sequences(data.question2.values.astype(str))
+x2 = sequence.pad_sequences(x2, maxlen=max_len)
+
+word_index = tk.word_index
+
+ytrain_enc = np_utils.to_categorical(y)
+
+embeddings_index = {}
+f = open('data/glove.840B.300d.txt')
+for line in tqdm(f):
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+print('Found %s word vectors.' % len(embeddings_index))
+
+embedding_matrix = np.zeros((len(word_index) + 1, 300))
+for word, i in tqdm(word_index.items()):
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        embedding_matrix[i] = embedding_vector
+
+max_features = 200000
+filter_length = 5
+nb_filter = 64
+pool_length = 4
+
+model = Sequential()
+print('Build model...')
+
+model1 = Sequential()
+model1.add(Embedding(len(word_index) + 1,
+                     300,
+                     weights=[embedding_matrix],
+                     input_length=40,
+                     trainable=False))
+
+model1.add(TimeDistributed(Dense(300, activation='relu')))
+model1.add(Lambda(lambda x: K.sum(x, axis=1), output_shape=(300,)))
+
+model2 = Sequential()
+model2.add(Embedding(len(word_index) + 1,
+                     300,
+                     weights=[embedding_matrix],
+                     input_length=40,
+                     trainable=False))
+
+model2.add(TimeDistributed(Dense(300, activation='relu')))
+model2.add(Lambda(lambda x: K.sum(x, axis=1), output_shape=(300,)))
+
+model3 = Sequential()
+model3.add(Embedding(len(word_index) + 1,
+                     300,
+                     weights=[embedding_matrix],
+                     input_length=40,
+                     trainable=False))
+model3.add(Convolution1D(nb_filter=nb_filter,
+                         filter_length=filter_length,
+                         border_mode='valid',
+                         activation='relu',
+                         subsample_length=1))
+model3.add(Dropout(0.2))
+
+model3.add(Convolution1D(nb_filter=nb_filter,
+                         filter_length=filter_length,
+                         border_mode='valid',
+                         activation='relu',
+                         subsample_length=1))
+
+model3.add(GlobalMaxPooling1D())
+model3.add(Dropout(0.2))
+
+model3.add(Dense(300))
+model3.add(Dropout(0.2))
+model3.add(BatchNormalization())
+
+model4 = Sequential()
+model4.add(Embedding(len(word_index) + 1,
+                     300,
+                     weights=[embedding_matrix],
+                     input_length=40,
+                     trainable=False))
+model4.add(Convolution1D(nb_filter=nb_filter,
+                         filter_length=filter_length,
+                         border_mode='valid',
+                         activation='relu',
+                         subsample_length=1))
+model4.add(Dropout(0.2))
+
+model4.add(Convolution1D(nb_filter=nb_filter,
+                         filter_length=filter_length,
+                         border_mode='valid',
+                         activation='relu',
+                         subsample_length=1))
+
+model4.add(GlobalMaxPooling1D())
+model4.add(Dropout(0.2))
+
+model4.add(Dense(300))
+model4.add(Dropout(0.2))
+model4.add(BatchNormalization())
+model5 = Sequential()
+model5.add(Embedding(len(word_index) + 1, 300, input_length=40, dropout=0.2))
+model5.add(LSTM(300, dropout_W=0.2, dropout_U=0.2))
+
+model6 = Sequential()
+model6.add(Embedding(len(word_index) + 1, 300, input_length=40, dropout=0.2))
+model6.add(LSTM(300, dropout_W=0.2, dropout_U=0.2))
+
+merged_model = Sequential()
+merged_model.add(Merge([model1, model2, model3, model4, model5, model6], mode='concat'))
+merged_model.add(BatchNormalization())
+
+merged_model.add(Dense(300))
+merged_model.add(PReLU())
+merged_model.add(Dropout(0.2))
+merged_model.add(BatchNormalization())
+
+merged_model.add(Dense(300))
+merged_model.add(PReLU())
+merged_model.add(Dropout(0.2))
+merged_model.add(BatchNormalization())
+
+merged_model.add(Dense(300))
+merged_model.add(PReLU())
+merged_model.add(Dropout(0.2))
+merged_model.add(BatchNormalization())
+
+merged_model.add(Dense(300))
+merged_model.add(PReLU())
+merged_model.add(Dropout(0.2))
+merged_model.add(BatchNormalization())
+
+merged_model.add(Dense(300))
+merged_model.add(PReLU())
+merged_model.add(Dropout(0.2))
+merged_model.add(BatchNormalization())
+
+merged_model.add(Dense(1))
+merged_model.add(Activation('sigmoid'))
+
+merged_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+checkpoint = ModelCheckpoint('weights.h5', monitor='val_acc', save_best_only=True, verbose=2)
+
+merged_model.fit([x1, x2, x1, x2, x1, x2], y=y, batch_size=384, nb_epoch=200,
+                 verbose=1, validation_split=0.1, shuffle=True, callbacks=[checkpoint])
---
+++ feature_engineering.py
@@ -0,0 +1,122 @@
+"""
+Detecting duplicate quora questions
+feature engineering
+@author: Abhishek Thakur
+"""
+
+import cPickle
+import pandas as pd
+import numpy as np
+from fuzzywuzzy import fuzz
+from gensim.models import Word2Vec
+from nltk.corpus import stopwords
+from tqdm import tqdm
+from scipy.stats import skew, kurtosis
+from scipy.spatial.distance import cosine, cityblock, jaccard, canberra, euclidean, minkowski, braycurtis
+from nltk import word_tokenize
+stop_words = stopwords.words('english')
+
+
+def wmd(s1, s2):
+    s1 = str(s1).lower().split()
+    s2 = str(s2).lower().split()
+    stop_words = stopwords.words('english')
+    s1 = [w for w in s1 if w not in stop_words]
+    s2 = [w for w in s2 if w not in stop_words]
+    return model.wmdistance(s1, s2)
+
+
+def norm_wmd(s1, s2):
+    s1 = str(s1).lower().split()
+    s2 = str(s2).lower().split()
+    stop_words = stopwords.words('english')
+    s1 = [w for w in s1 if w not in stop_words]
+    s2 = [w for w in s2 if w not in stop_words]
+    return norm_model.wmdistance(s1, s2)
+
+
+def sent2vec(s):
+    words = str(s).lower().decode('utf-8')
+    words = word_tokenize(words)
+    words = [w for w in words if not w in stop_words]
+    words = [w for w in words if w.isalpha()]
+    M = []
+    for w in words:
+        try:
+            M.append(model[w])
+        except:
+            continue
+    M = np.array(M)
+    v = M.sum(axis=0)
+    return v / np.sqrt((v ** 2).sum())
+
+
+data = pd.read_csv('quora_duplicate_questions.tsv', sep='\t')
+data = data.drop(['id', 'qid1', 'qid2'], axis=1)
+
+
+data['len_q1'] = data.question1.apply(lambda x: len(str(x)))
+data['len_q2'] = data.question2.apply(lambda x: len(str(x)))
+data['diff_len'] = data.len_q1 - data.len_q2
+data['len_char_q1'] = data.question1.apply(lambda x: len(''.join(set(str(x).replace(' ', '')))))
+data['len_char_q2'] = data.question2.apply(lambda x: len(''.join(set(str(x).replace(' ', '')))))
+data['len_word_q1'] = data.question1.apply(lambda x: len(str(x).split()))
+data['len_word_q2'] = data.question2.apply(lambda x: len(str(x).split()))
+data['common_words'] = data.apply(lambda x: len(set(str(x['question1']).lower().split()).intersection(set(str(x['question2']).lower().split()))), axis=1)
+data['fuzz_qratio'] = data.apply(lambda x: fuzz.QRatio(str(x['question1']), str(x['question2'])), axis=1)
+data['fuzz_WRatio'] = data.apply(lambda x: fuzz.WRatio(str(x['question1']), str(x['question2'])), axis=1)
+data['fuzz_partial_ratio'] = data.apply(lambda x: fuzz.partial_ratio(str(x['question1']), str(x['question2'])), axis=1)
+data['fuzz_partial_token_set_ratio'] = data.apply(lambda x: fuzz.partial_token_set_ratio(str(x['question1']), str(x['question2'])), axis=1)
+data['fuzz_partial_token_sort_ratio'] = data.apply(lambda x: fuzz.partial_token_sort_ratio(str(x['question1']), str(x['question2'])), axis=1)
+data['fuzz_token_set_ratio'] = data.apply(lambda x: fuzz.token_set_ratio(str(x['question1']), str(x['question2'])), axis=1)
+data['fuzz_token_sort_ratio'] = data.apply(lambda x: fuzz.token_sort_ratio(str(x['question1']), str(x['question2'])), axis=1)
+
+
+model = Word2Vec.load_word2vec_format('data/GoogleNews-vectors-negative300.bin.gz', binary=True)
+data['wmd'] = data.apply(lambda x: wmd(x['question1'], x['question2']), axis=1)
+
+
+norm_model = Word2Vec.load_word2vec_format('data/GoogleNews-vectors-negative300.bin.gz', binary=True)
+norm_model.init_sims(replace=True)
+data['norm_wmd'] = data.apply(lambda x: norm_wmd(x['question1'], x['question2']), axis=1)
+
+question1_vectors = np.zeros((data.shape[0], 300))
+error_count = 0
+
+for i, q in tqdm(enumerate(data.question1.values)):
+    question1_vectors[i, :] = sent2vec(q)
+
+question2_vectors  = np.zeros((data.shape[0], 300))
+for i, q in tqdm(enumerate(data.question2.values)):
+    question2_vectors[i, :] = sent2vec(q)
+
+data['cosine_distance'] = [cosine(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+
+data['cityblock_distance'] = [cityblock(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+
+data['jaccard_distance'] = [jaccard(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+
+data['canberra_distance'] = [canberra(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+
+data['euclidean_distance'] = [euclidean(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+
+data['minkowski_distance'] = [minkowski(x, y, 3) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+
+data['braycurtis_distance'] = [braycurtis(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+
+data['skew_q1vec'] = [skew(x) for x in np.nan_to_num(question1_vectors)]
+data['skew_q2vec'] = [skew(x) for x in np.nan_to_num(question2_vectors)]
+data['kur_q1vec'] = [kurtosis(x) for x in np.nan_to_num(question1_vectors)]
+data['kur_q2vec'] = [kurtosis(x) for x in np.nan_to_num(question2_vectors)]
+
+cPickle.dump(question1_vectors, open('data/q1_w2v.pkl', 'wb'), -1)
+cPickle.dump(question2_vectors, open('data/q2_w2v.pkl', 'wb'), -1)
+
+data.to_csv('data/quora_features.csv', index=False)
